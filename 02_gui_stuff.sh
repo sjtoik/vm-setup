aef95df1d084612ca5e29eca836daf133082d5ca
Santeri Toikka <586403+sjtoik@users.noreply.github.com>
2018-04-22 21:22:37 +0300

    Backlight control.

--- 02_gui_stuff.sh
+++ 02_gui_stuff.sh
@@ -2,7 +2,16 @@
 set -ex
 
 sudo apt update
-sudo apt install -y i3
+sudo apt install -y i3 
 
 install -d ~/.config/i3
 cp ./i3.conf ~/.config/i3/config
+
+# backlight control
+sudo apt install -y git help2man
+git clone https://github.com/haikarainen/light
+cd light
+make
+sudo make install
+cd ..
+rm -fr light

ef08358e3f8257153fbe0cbac09e53e77a99aba9
Santeri Toikka <586403+sjtoik@users.noreply.github.com>
2018-10-28 09:48:03 +0200

    i3 configs

--- 02_gui_stuff.sh
+++ 02_gui_stuff.sh
@@ -6,6 +6,8 @@ sudo apt install -y i3 suckless-tools chromium chromium-browser
 
 install -d ~/.config/i3
 cp ./i3.conf ~/.config/i3/config
+cp ./i3_lockoff.sh ~/.config/i3/
+cp ./backgrounds/34C3_Logotype_Plus.png ~/.config/i3/config/background.png
 
 # backlight control
 sudo apt install -y git help2man
---
+++ i3_lockoff.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+i3lock -c 000000 -i ~/.config/i3/background.png
+sleep 3
+xset dpms force off

2c7f90d5c469c867428696c6841515523988bd68
Santeri Toikka <586403+sjtoik@users.noreply.github.com>
2020-02-26 10:09:12 +0200

    Sway updates.

--- 02_sway.sh
+++ 02_sway.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 set -ex
 
-sudo add-apt-repository -y ppa:samoilov-lex/sway
 sudo apt update
-sudo apt install sway swaylock swayidle swaybg rofi
+sudo apt install -y sway swaylock swayidle swaybg i3status
 
 cp -r ./sway ~/.config/
-
-install -d ~/.config/rofi
-cp ./rofi.conf ~/.config/rofi/config
 
 # backlight control
 sudo apt install -y git help2man dh-autoreconf
--- 03_keybase.sh
+++ 03_keybase.sh
@@ -3,10 +3,8 @@ set -x
 
 # keybase
 sudo apt install -y curl
-sudo apt autoremove -y
-curl -O https://prerelease.keybase.io/keybase_amd64.deb
-sudo dpkg -i keybase_amd64.deb
-sudo apt-get install -f -y
+curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
+sudo apt install -y ./keybase_amd64.deb
 run_keybase
 rm keybase_amd64.deb
 

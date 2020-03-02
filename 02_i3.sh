5c52e011ce4e77bb6e933f467a4c753f4aeead2e
Santeri Toikka <586403+sjtoik@users.noreply.github.com>
2020-03-02 21:53:07 +0200

    Changes from drake.

---
+++ 02_i3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+sudo apt update
+sudo apt autoremove -y
+sudo apt install -y i3 suckless-tools net-tools
+
+cp -r ./i3 ~/.config/
+
+# backlight control
+sudo apt install -y git help2man dh-autoreconf
+git clone https://github.com/haikarainen/light
+cd light
+./autogen.sh
+./configure
+make
+sudo make install
+cd ..
+rm -fr light
--- ubuntu_lime.sh
+++ ubuntu_lime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y limesuite liblimesuite-dev limesuite-udev limesuite-images
+sudo apt-get install -y limesuite liblimesuite-dev limesuite-udev 
 sudo apt-get install -y soapysdr-module-lms7
 
 sudo cp ubuntu_lime.rules /etc/udev/rules.d/64-limesuite.rules

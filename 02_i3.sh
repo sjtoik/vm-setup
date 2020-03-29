60959e0d4db3b9bab6722a9b6dd4bd3f1f566bba
Santeri Toikka <586403+sjtoik@users.noreply.github.com>
2020-03-29 16:07:28 +0300

    i3 for intgrated nvidia

--- 02_i3.sh
+++ 02_i3.sh
@@ -6,6 +6,7 @@ sudo apt autoremove -y
 sudo apt install -y i3 suckless-tools net-tools
 
 cp -r ./i3 ~/.config/
+cp ./i3/.Xresources ~/
 
 # backlight control
 sudo apt install -y git help2man dh-autoreconf

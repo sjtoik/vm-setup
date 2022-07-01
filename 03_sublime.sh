549d97e93e61ea5d756b2aadaa28e7da6e8fe79c
Santeri Toikka <586403+sjtoik@users.noreply.github.com>
2022-07-01 10:26:15 +0300

    Wincmd updates

--- 03_sublime.sh
+++ 03_sublime.sh
@@ -4,10 +4,9 @@ set -ex
 #sublime
 sudo apt autoremove -y
 sudo apt install -y wget
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
-sudo apt-get install -y apt-transport-https
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
+sudo apt install -y apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt update
+sudo apt install sublime-text
 

f3e0b45b3c9ee32bda30a205656a408180d40489
Santeri Toikka <santeri.toikka@iki.fi>
2023-12-10 15:05:38 +0200

    Updated background setter to deps

--- 02_i3.sh
+++ 02_i3.sh
@@ -3,7 +3,7 @@ set -ex
 
 sudo apt update
 sudo apt autoremove -y
-sudo apt install -y i3 suckless-tools net-tools
+sudo apt install -y i3 suckless-tools net-tools feh
 
 cp -r ./i3 ~/.config/
 cp ./i3/.Xresources ~/
